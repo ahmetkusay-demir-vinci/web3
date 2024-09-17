63604698458689ccbcac852cd81d30ae325deba0
ahmetkusay-demir-vinci <128645290+ahmetkusay-demir-vinci@users.noreply.github.com>
2024-09-17 22:10:29 +0200

    1.1-1.2 : refactor - modules

---
+++ exercices/module1/1.1-1.2/src/components/App/App.jsx
@@ -0,0 +1,25 @@
+import Content from "../Content/Content"
+import Header from "../Header/header"
+import Total from "../Total/Total"
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </>
+  );
+};
+
+console.log('Tout est bon !');
+
+export default App;
