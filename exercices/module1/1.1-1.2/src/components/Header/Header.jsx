c49a081e3106e3a6c804658818dfdf4676e1b475
ahmetkusay-demir-vinci <128645290+ahmetkusay-demir-vinci@users.noreply.github.com>
2024-09-17 23:00:34 +0200

    1.1-1.2 : import image

--- exercices/module1/1.1-1.2/src/components/Header/Header.jsx
+++ exercices/module1/1.1-1.2/src/components/Header/Header.jsx
@@ -1,6 +1,11 @@
+import logo from './LOGO HE VINCI.png';
+
 const Header = (props) => {
     return (
-        <h1>{props.course}</h1>
+        <div>
+            <img src={logo} alt="logo Vinci"></img>
+            <h1>{props.course}</h1>
+        </div>   
     );
 };
 
