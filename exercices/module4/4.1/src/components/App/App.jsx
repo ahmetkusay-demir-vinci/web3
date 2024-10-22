adfc67c014ebc434c902b05613ece95256818a01
ahmetkusay-demir-vinci <128645290+ahmetkusay-demir-vinci@users.noreply.github.com>
2024-10-22 14:18:42 +0200

    add: 4.1

---
+++ exercices/module4/4.1/src/components/App/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  // Fonction pour gérer la soumission du formulaire
+  const addPerson = (event) => {
+    event.preventDefault(); // Empêcher le rechargement de la page
+    const personObject = {
+      name: newName,
+    };
+
+    // Ajouter la nouvelle personne au tableau
+    setPersons(persons.concat(personObject));
+    setNewName(''); // Réinitialiser l'entrée du formulaire
+  };
+
+  // Fonction pour capturer les changements dans l'entrée
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) => (
+          <li key={index}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
