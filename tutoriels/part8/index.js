f01f67ed435e3792397458f2bfde4d245c4f79c9
ahmetkusay-demir-vinci <128645290+ahmetkusay-demir-vinci@users.noreply.github.com>
2024-12-10 14:45:30 +0100

    add: part8 mutation editNumber

--- tutoriels/part8/index.js
+++ tutoriels/part8/index.js
@@ -1,28 +1,28 @@
-const {ApolloServer} = require('@apollo/server');
-const {startStandaloneServer} = require('@apollo/server/standalone');
-const {v1: uuid} = require('uuid');
-const {GraphQLError} = require('graphql');
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const { v1: uuid } = require('uuid');
+const { GraphQLError } = require('graphql');
 
 let persons = [
   {
-    name: "Arto Hellas",
-    phone: "040-123543",
-    street: "Tapiolankatu 5 A",
-    city: "Espoo",
-    id: "3d594650-3436-11e9-bc57-8b80ba54c431",
+    name: 'Arto Hellas',
+    phone: '040-123543',
+    street: 'Tapiolankatu 5 A',
+    city: 'Espoo',
+    id: '3d594650-3436-11e9-bc57-8b80ba54c431',
   },
   {
-    name: "Matti Luukkainen",
-    phone: "040-432342",
-    street: "Malminkaari 10 A",
-    city: "Helsinki",
-    id: "3d599470-3436-11e9-bc57-8b80ba54c431",
+    name: 'Matti Luukkainen',
+    phone: '040-432342',
+    street: 'Malminkaari 10 A',
+    city: 'Helsinki',
+    id: '3d599470-3436-11e9-bc57-8b80ba54c431',
   },
   {
-    name: "Venla Ruuska",
-    street: "Nallemäentie 22 C",
-    city: "Helsinki",
-    id: "3d599471-3436-11e9-bc57-8b80ba54c431",
+    name: 'Venla Ruuska',
+    street: 'Nallemäentie 22 C',
+    city: 'Helsinki',
+    id: '3d599471-3436-11e9-bc57-8b80ba54c431',
   },
 ];
 
@@ -57,25 +57,28 @@ const typeDefs = `
       street: String!
       city: String!
     ): Person
+    editNumber(
+      name: String!
+      phone: String!
+    ): Person
   }
 `;
 
-// resolvers permettent de définir la logique de résolution des champs
 const resolvers = {
   Query: {
     personCount: () => persons.length,
     allPersons: (root, args) => {
       if (!args.phone) {
-        return persons
+        return persons;
       }
       const byPhone = (person) =>
-          args.phone === 'YES' ? person.phone : !person.phone
-      return persons.filter(byPhone)
+          args.phone === 'YES' ? person.phone : !person.phone;
+      return persons.filter(byPhone);
     },
     findPerson: (root, args) => persons.find((p) => p.name === args.name),
   },
   Person: {
-    address: ({street, city}) => ({
+    address: ({ street, city }) => ({
       street,
       city,
     }),
@@ -91,9 +94,21 @@ const resolvers = {
         });
       }
 
-      const person = {...args, id: uuid()};
+      const person = { ...args, id: uuid() };
       persons = persons.concat(person);
       return person;
+    },
+    editNumber: (root, args) => {
+      const person = persons.find((p) => p.name === args.name);
+      if (!person) {
+        return null;
+      }
+
+      const updatedPerson = { ...person, phone: args.phone };
+      persons = persons.map((p) =>
+          p.name === args.name ? updatedPerson : p
+      );
+      return updatedPerson;
     },
   },
 };
@@ -104,7 +119,7 @@ const server = new ApolloServer({
 });
 
 startStandaloneServer(server, {
-  listen: {port: 4000},
-}).then(({url}) => {
+  listen: { port: 4000 },
+}).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
