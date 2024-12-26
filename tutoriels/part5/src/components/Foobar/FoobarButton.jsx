a471a8f85bca1c1b57fad8db4cd02fe32b0ff6f1
ahmetkusay-demir-vinci <128645290+ahmetkusay-demir-vinci@users.noreply.github.com>
2024-12-26 15:59:28 +0100

    edit: part5 FoobarButton

--- tutoriels/part5/src/components/Foobar/FoobarButton.jsx
+++ tutoriels/part5/src/components/Foobar/FoobarButton.jsx
@@ -3,6 +3,15 @@ import { Context as LanguageContext } from "contexts/LanguageContext";
 
 const FoobarButton = () => {
   const { language, pickLanguage } = useContext(LanguageContext);
+
+  return (
+    <div>
+      <p>Current language: {language}</p>
+      <button onClick={() => pickLanguage(language === "fr" ? "en" : "fr")}>
+        Switch Language
+      </button>
+    </div>
+  );
 };
 
 export default FoobarButton;
